f67947532d34221723c9d3415268150e3cf9f70f
Subhabrata Khara <96125557+Subhabrata98@users.noreply.github.com>
2026-03-07 20:38:00 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+class Hello
+{   
+    public static void main(String a[])
+    {
+        int num1 = 3;
+        int num2 = 5;
+        int result = num1+num2;
+        float marks = 6.5f; 
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println("Result : " + result);
+        System.out.println(3+5);
+        System.out.println("Hello World");
+        System.out.println(marks);
+    }
+} 
