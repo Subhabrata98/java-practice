d248c3b5e25371782d7ce14fdb9674f85dc4ba04
Subhabrata Khara <96125557+Subhabrata98@users.noreply.github.com>
2026-03-08 22:12:52 +0530

    Add files via upload

---
+++ primitive.java
@@ -0,0 +1,23 @@
+class primitive {
+    public static void main(String args[])
+    {
+        byte a = 5;
+        short b = 10;
+        long c = 15;
+        int d = 20;
+        float e = 5.26f;
+        double f = 2.145675d;
+        char g = 'H';
+        boolean h = true;
+
+
+        System.out.println("Byte: " + a);
+        System.out.println("Short: " + b);
+        System.out.println("Long: " + c);
+        System.out.println("Int: " + d);
+        System.out.println("Float: " + e);
+        System.out.println("Double: " + f);
+        System.out.println("Char: " + g);
+        System.out.println("Boolean: " + h);
+    }
+}
