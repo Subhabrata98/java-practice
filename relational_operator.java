783398f1282083fa1f1ee730c186830a92a2bfbb
Subhabrata Khara <96125557+Subhabrata98@users.noreply.github.com>
2026-03-10 19:42:52 +0530

    Add files via upload

---
+++ relational_operator.java
@@ -0,0 +1,13 @@
+public class relational_operator {
+    public static void main(String args[]){
+
+        int x = 5;
+        int y =5;
+
+        boolean result = x != y;
+        boolean result1 = x == y;
+
+        System.out.println(result);
+        System.out.println(result1);
+    }
+}
