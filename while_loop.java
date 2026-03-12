dde4caa1d501de733f3d3873eb350a4b41c92507
Subhabrata Khara <96125557+Subhabrata98@users.noreply.github.com>
2026-03-12 14:16:07 +0530

    Add files via upload

---
+++ while_loop.java
@@ -0,0 +1,27 @@
+public class while_loop {
+    public static void main(String args[]){
+
+
+        // loop - while, do while, for
+
+        // 100 - condition
+        // while loop
+
+        int i = 1;
+
+        while(i<=4)
+        {
+            System.out.println("Hi " + i);
+            int j = 1;
+            while(j<=3){
+                System.out.println("Hello " + j);
+                j++;
+            }
+            i++;
+        }
+
+        System.out.println("Bye " + i);
+
+    }
+    
+}
