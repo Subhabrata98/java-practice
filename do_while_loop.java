773237c431e8ffdc026799b86fc89e0db495e2eb
Subhabrata Khara <96125557+Subhabrata98@users.noreply.github.com>
2026-03-13 11:22:22 +0530

    Add files via upload

---
+++ do_while_loop.java
@@ -0,0 +1,11 @@
+public class do_while_loop {
+    public static void main(String args[]){
+
+        int i = 5;
+
+        do{  // in case of while loop it only prints if the range satissfies
+            // in do while even if its out of rance it atleat prints one outcome, so user can know
+            System.out.println("Hi " + i);
+        }while(i<=4);
+    }
+}
