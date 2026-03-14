a794c5be49075a770f384f1a3c4bd6790aa55914
Subhabrata Khara <96125557+Subhabrata98@users.noreply.github.com>
2026-03-14 19:12:33 +0530

    Add files via upload

---
+++ loop_practise.java
@@ -0,0 +1,13 @@
+public class loop_practise {
+    public static void main(String args[]){
+
+    for(int i=1; i<=5; i++){
+        System.out.println("Day: " +  i);
+
+
+    for(int j=9; j<=16; j++){
+            System.out.println(j);
+    }
+}
+}
+}
