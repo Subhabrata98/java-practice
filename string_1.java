0782f21f10665cb6ed2a80eebb922d2ff0252d02
Subhabrata Khara <96125557+Subhabrata98@users.noreply.github.com>
2026-03-20 20:31:29 +0530

    Add files via upload

---
+++ string_1.java
@@ -0,0 +1,8 @@
+public class string_1 {
+    public static void main(String args[]){
+
+        String name = new String("Subhabrata");
+        System.out.println(name);
+        System.out.println("Hello" + " " + name);
+    }
+}
