1b5d293a266fb044c8ed78d05ef1b921f3b71db8
Subhabrata Khara <96125557+Subhabrata98@users.noreply.github.com>
2026-03-21 15:44:07 +0530

    Add files via upload

--- string_1.java
+++ string_1.java
@@ -1,8 +1,12 @@
 public class string_1 {
     public static void main(String args[]){
 
-        String name = new String("Subhabrata");
-        System.out.println(name);
+        
+        // String name = new String("Subhabrata");
+        String name = "Subhabrata"; // we can simply use this. this will auto create object for string
+        // System.out.println(name);
         System.out.println("Hello" + " " + name);
+        // System.out.println(name.charAt(1));// know the character at this index place
+        // System.out.println(name.concat(" " + "Khara"));//concatinating  
     }
 }
