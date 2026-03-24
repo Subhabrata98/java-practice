81abca95f1a084852e6c66f8e6411e95d90b8975
Subhabrata Khara <96125557+Subhabrata98@users.noreply.github.com>
2026-03-24 11:38:19 +0530

    Add files via upload

--- constructor_1.java
+++ constructor_1.java
@@ -19,10 +19,16 @@ public void setAge(int age){
     this.age = age;
 }
 
-public Human(){ // this is a constructor, same class name, no return type, only public class
+public Human(){ // this is a default constructor, same class name, no return type, only public class
 
     age = 18;
     name = "John";
+}
+
+public Human(int age, String name){ // parameterized constructor
+
+    this.age = age;
+    this.name = name;
 }
 
 }
@@ -31,8 +37,15 @@ public class constructor_1 {
     public static void main(String args[]){
 
     Human obj = new Human();
+    Human obj1 = new Human(27,"Subhabrata");
+    Human obj2 = new Human();
+    obj2.setName("abc");
+    obj2.setAge(15);
+    
 
-    System.out.println(obj.getName() + " : " + obj.getAge()); // if we call this we will get null : 0, whhich is tthe default value
+    System.out.println(obj.getName() + " : " + obj.getAge()); // if we call this we will get null : 0, whhich is the default value
+    System.out.println(obj1.getName() + " : " + obj1.getAge());
+    System.out.println(obj2.getName() + " : " + obj2.getAge());
     //so we can use constructor to set the default value to another we want
     // it is simar to method, must have same class name, no returntype required as it returns nothing
     // obj.setAge(27);
