6615441cad65dc9d8cecbad847868130ad56534f
Subhabrata Khara <96125557+Subhabrata98@users.noreply.github.com>
2026-03-27 15:38:26 +0530

    Add files via upload

---
+++ inheritance_2.java
@@ -0,0 +1,14 @@
+public class inheritance_2 extends inheritance_1{ // inheritance_2 is clild class and inheritance_1 is the parent class
+
+
+    public int mul(int n1, int n2)
+    {
+        return n1 * n2;
+    }
+    public int div(int n1, int n2)
+    {
+        return n1 / n2;
+    }
+
+
+}
