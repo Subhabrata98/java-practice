7712aaf58107e4b10e5ca0b7e2168158c066777b
Subhabrata Khara <96125557+Subhabrata98@users.noreply.github.com>
2026-05-03 23:10:32 +0530

    Add files via upload

---
+++ pattern_28.java
@@ -0,0 +1,22 @@
+public class pattern_28 {
+
+    public static void main(String[] args) {
+
+        int n = 6;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+
+            }
+
+            System.out.println();
+        }
+    }
+}
